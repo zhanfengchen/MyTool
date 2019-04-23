9ab80f92f1928299e4b740121daca63540d29720
frey.chen <frey.chen@hirede.com>
2019-04-23 19:45:04 +0800

    add first test

--- grid_world_game/src/environment.py
+++ grid_world_game/src/environment.py
@@ -3,14 +3,33 @@
 
 class Environment(object):
     def __init__(self, W, H):
-        self._A = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # 按照顺时针，上、左、下、右移动。
         self._W = W
         self._H = H
-        self._end_set = set([(0, 0), (W-1, H-1)])
+        self._end_set = set([(0, 0), (W - 1, H - 1)])
+
+        self._pointers = [(x, y)
+                          for x in range(W)
+                          for y in range(H)]
+
+        self._actions = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # 按照顺时针，上、左、下、右移动。
+        self._action_prob_mapping = {p: {a: 1.0 / len(self._actions)
+                                         for a in self._actions}
+                                     for p in self._pointers}
+    @property
+    def end_set(self):
+        return self._end_set
+
+    @property
+    def actions(self):
+        return self._actions
+
+    @property
+    def pointers(self):
+        return self._pointers
 
     @property
     def nA(self):
-        return len(self._A)
+        return len(self._actions)
 
     @property
     def W(self):
@@ -22,7 +41,9 @@ class Environment(object):
 
     def move(self, status, action):
         x, y = status
-        x_off, y_off = self._A[action]
+        x_off, y_off = action
         next_status = (x + x_off, y + y_off)
         done = next_status in self._end_set
-        return next_status, done
+
+        action_prob = self._action_prob_mapping.get(status, {}).get(action, 0.)
+        return next_status, action_prob, done
--- grid_world_game/src/grid_RL.py
+++ grid_world_game/src/grid_RL.py
@@ -1,13 +1,39 @@
 # -*- coding:utf-8 -*-
+import math
 import numpy as np
 from environment import Environment
 
 
-envr = Environment(4, 4)
+def itertools(envr, status, actions, status_reward_mtx, discount):
+    rewards = []
+    for action in actions:
+        next_status, action_prob, done = envr.move(status, action)
+        rewards.append(action_prob * (status_reward_mtx[status] + discount*status_reward_mtx.get(next_status, 0.)))
+    return rewards
 
 
-def move(envr, status, action, status_reward_mtx):
-    pass
+def rl(envr, threshold=0.00001):
+    pointers = envr.pointers
+    actions = envr.actions
+    V = {p: 0.0 for p in pointers}
+    for p in envr.end_set:
+        V[p] = 1.
 
+    ##
+    last_value = 0.0
+    while True:
+        for p in pointers:
+            rewards = itertools(envr, p, actions, V, 1.0)
+            max_rewards = np.max(rewards)
+            V[p] = max_rewards
+        if abs(last_value - sum(V.values())) < threshold:
+            break
+    return V
 
-def rl(, threshold=0.001):
+if __name__ == '__main__':
+    envr = Environment(4, 4)
+    V = rl(envr)
+    for i in range(4):
+        print "\n"
+        for j in range(4):
+            print V[(i, j)],
