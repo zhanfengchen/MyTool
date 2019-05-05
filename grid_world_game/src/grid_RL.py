b78588a76d706ce2872c2e7d95f734201637d563
1607350868@qq.com <1607350868@qq.com>
2019-05-05 20:25:05 +0800

    update grid_world_game

--- grid_world_game/src/grid_RL.py
+++ grid_world_game/src/grid_RL.py
@@ -8,32 +8,43 @@ def itertools(envr, status, actions, status_reward_mtx, discount):
     rewards = []
     for action in actions:
         next_status, action_prob, done = envr.move(status, action)
-        rewards.append(action_prob * (status_reward_mtx[status] + discount*status_reward_mtx.get(next_status, 0.)))
+        next_status_reward = status_reward_mtx.get(next_status, 0. if status in envr.end_set else -2.)
+        value = action_prob * (status_reward_mtx[status] + discount * next_status_reward)
+        rewards.append((action, value))
     return rewards
 
 
-def rl(envr, threshold=0.00001):
+def rl(envr, threshold=0.0000001):
     pointers = envr.pointers
     actions = envr.actions
-    V = {p: 0.0 for p in pointers}
+    V = {p: -1.0 for p in pointers}
     for p in envr.end_set:
-        V[p] = 1.
+        V[p] = 0.0
 
     ##
     last_value = 0.0
+    v_actions = {p: 0 for p in pointers}
     while True:
         for p in pointers:
             rewards = itertools(envr, p, actions, V, 1.0)
-            max_rewards = np.max(rewards)
-            V[p] = max_rewards
+            ac, max_reward = max(rewards, key=lambda x: x[1])
+            V[p] = max_reward
+            v_actions[p] = ac
         if abs(last_value - sum(V.values())) < threshold:
             break
-    return V
+    return V, v_actions
+
 
 if __name__ == '__main__':
     envr = Environment(4, 4)
-    V = rl(envr)
+    V, ac = rl(envr)
     for i in range(4):
         print "\n"
         for j in range(4):
             print V[(i, j)],
+
+    dir_mapping = dict(zip([(-1, 0), (0, 1), (1, 0), (0, -1)], [u"上", u"左", u"下", u"右", ]))
+    for i in range(4):
+        print "\n"
+        for j in range(4):
+            print dir_mapping[ac[(i, j)]],
